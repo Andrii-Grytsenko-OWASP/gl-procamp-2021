0f4f549163392721ff83d1652cd1d0deb09b36f6
Andrii Grytsenko <Andrii.Grytsenko@grytsenko.WEBSITE>
2021-12-11 21:37:06 +0200

    Refactoring: replace hardcoded strings by enums

---
+++ src/enums.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class WebDrivers(Enum):
+    WD_CHROME = "Chrome"
+    WD_FIREFOX = "Firefox"
--- src/tests/fixtures/fixture_config.py
+++ src/tests/fixtures/fixture_config.py
@@ -2,11 +2,12 @@ from pytest import *
 from selenium import webdriver
 
 from src import config as cfg
+from src.enums import *
 
 
 @fixture(scope="class")
 def webdriver():
-    if cfg.WEB_DRIVER == "Chrome":
+    if cfg.WEB_DRIVER.capitalize() == WebDrivers.WD_CHROME:
         wd = webdriver.Chrome
     else:
         wd = webdriver.Firefox
